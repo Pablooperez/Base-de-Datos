0e9eb505792e1e9b1641d902070625648fd5f542
Pablo <pablo.perez.martinez99@gmail.com>
2026-03-25 18:32:32 +0100

    Practica cursores

--- BBDD/dbSupermercado_Crusores.sql
+++ BBDD/dbSupermercado_Crusores.sql
@@ -195,6 +195,7 @@ begin
     select tblproductos.codigobarra ,tblproductos.precio, tblproductos.precio_venta, tblproductos.iva, tblproductos.cantidad
     from dbsupermercado.tblproductos;
     declare continue handler for not found set v_fin = true;
+    set v_totaliva0=0, v_totaliva4=0, v_totaliva10=0, v_totaliva21=0;
     open cur_Ganancias;
     ganancias: LOOP
         fetch cur_Ganancias into v_codigobarra, v_precio, v_precioVenta, v_iva;
@@ -214,5 +215,194 @@ select @v_totaliva0 as totalBeneficioIVA0, @v_totaliva4 as totalBeneficiosIVA4, 
 call dbSupermercado.cGananciasIva(@v_totaliva0, @v_totaliva4, @v_totaliva10, @v_totaliva21);
 
 /* GANANCIAS POR TIENDA */
+
+/* EJERCICIOS PRACTICAR: Nivel 1: El "Hola Mundo" de los Cursores
+Objetivo: Aprender a declarar, abrir, recorrer y cerrar un cursor sin lógica compleja.
+Reto: Crea un procedimiento llamado ListarNombresProductos. Solo quiero que el cursor recorra la tabla tblproductos y guarde en una variable el nombre del último producto procesado.
+Pista: Solo necesitas una variable v_nombre y un FETCH simple.
+ */
+
+drop procedure if exists dbsupermercado.pListarNombreProductos;
+delimiter $$
+create procedure dbsupermercado.pListarNombreProductos()
+begin
+    declare v_fin boolean default false;
+    declare v_nombre varchar(100);
+    declare puntero cursor for select tblProductos.producto from dbsupermercado.tblProductos;
+    declare continue handler for not found set v_fin = true;
+    open puntero;
+    bucle: loop
+        fetch puntero into v_nombre;
+        if  v_fin then leave bucle;
+        end if;
+    end loop;
+    close puntero;
+
+    select v_nombre as ElUltimoNombreLeido;
+end $$
+delimiter $$
+
+call dbSupermercado.pListarNombreProductos();
+
+/* Nivel 2: El Contador de Productos "Caros"
+Tu misión:
+Crea un procedimiento llamado ContarProductosCaros. Queremos que el cursor recorra la tabla y,
+   cada vez que encuentre un producto cuyo precio de venta sea mayor a 50€, lo cuente.
+   Al final, el procedimiento nos dirá cuántos productos cumplen esa condición. */
+
+drop procedure if exists dbsupermercado.pContadorProductosCaros;
+delimiter $$
+create procedure dbsupermercado.pContadorProductosCaros()
+begin
+    declare v_fin boolean default false;
+    declare v_contador int default 0;
+    declare v_precio float;
+    declare puntero cursor for select tblProductos.precio_venta from dbsupermercado.tblProductos;
+    declare continue handler for not found set v_fin = true;
+    open puntero;
+    bucle: loop
+        fetch puntero into v_precio;
+        if v_fin then leave bucle;
+        end if;
+        if  v_precio > 5 then
+            set v_contador = v_contador + 1;
+        end if ;
+    end loop;
+    close puntero;
+    select v_contador as Cantidad;
+end $$
+delimiter ;
+
+call dbSupermercado.pContadorProductosCaros();
+
+/* Crea un procedimiento almacenado llamado pContarStockBajo. El objetivo es que el cursor recorra
+   la tabla tblproductos y cuente cuántos productos tienen una cantidad (stock) inferior a 10 unidades.
+   Al finalizar el recorrido, el procedimiento debe mostrar el total de productos encontrados que cumplen esa condición. */
+
+drop procedure if exists dbsupermercado.pContarStockBajo;
+delimiter $$
+create procedure dbsupermercado.pContarStockBajo()
+begin
+    declare v_fin boolean default false;
+    declare v_cantidad int;
+    declare v_contador int default 0;
+    declare puntero cursor for select tblProductos.cantidad from dbsupermercado.tblProductos;
+    declare continue handler for not found set v_fin = true;
+    open puntero;
+    bucle: loop
+        fetch puntero into v_cantidad;
+        if  v_fin then
+            leave bucle;
+        end if ;
+        if v_cantidad < 10 then
+            set v_contador = v_contador + 1;
+        end if ;
+    end loop ;
+    close puntero;
+    select v_contador as Cantidad;
+
+end $$
+delimiter ;
+
+call dbSupermercado.pContarStockBajo();
+
+/* Crea un procedimiento llamado pStockPorCategoria.
+Debe recibir un parámetro de entrada: IN p_id_categoria INT.
+El cursor debe seleccionar la cantidad de la tabla tblproductos,
+pero filtrando por esa categoría (es decir: WHERE idcategoria = p_id_categoria).
+El procedimiento debe sumar todas las cantidades de esa categoría y mostrar el total al final. */
+
+drop procedure if exists dbsupermercado.pStockPorCategoria;
+delimiter $$
+create procedure dbsupermercado.pStockPorCategoria(in v_categoria tinyint)
+begin
+    declare v_fin boolean default false;
+    declare v_cantidad int;
+    declare v_cantidad_total int default 0;
+    declare puntero cursor for select tblProductos.cantidad
+        from dbsupermercado.tblProductos
+            where idcategoría = v_categoria;
+    declare continue handler for not found set v_fin = true;
+    open puntero;
+    bucle: loop
+        fetch puntero into v_cantidad;
+        if v_fin then
+            leave bucle;
+        end if ;
+        set v_cantidad_total=v_cantidad_total+v_cantidad;
+    end loop ;
+    close puntero;
+    select v_cantidad_total as CantidadTotal;
+end $$
+delimiter ;
+
+call dbSupermercado.pStockPorCategoria(3);
+
+/* Crea un procedimiento almacenado llamado pLiquidarStockAlto. El objetivo es que el cursor
+   recorra la tabla tblproductos y, para cada producto que tenga una cantidad (stock) superior a 200 unidades,
+   le aplique automáticamente un descuento del 15% en su precio_venta. */
+
+drop procedure if exists dbsupermercado.pLiquidarStockAlto;
+delimiter $$
+create procedure dbSupermercado.pLiquidarStockAlto()
+begin
+    declare v_fin boolean default false;
+    declare v_cantidad int;
+    declare v_codigo int;
+    declare puntero cursor for select tblProductos.cantidad, tblProductos.codigobarra
+        from dbsupermercado.tblProductos
+            where cantidad > 20;
+    declare continue handler for not found set v_fin = true;
+    open puntero;
+    bucle: loop
+        fetch puntero into v_cantidad, v_codigo;
+        if v_fin then
+            leave bucle;
+        end if ;
+        update tblProductos
+        set precio_venta = tblProductos.precio_venta * 0.85
+        where codigobarra = v_codigo;
+    end loop ;
+    close puntero;
+end $$
+delimiter ;
+
+call dbSupermercado.pLiquidarStockAlto();
+
+/* Crea un procedimiento almacenado llamado pCorregirPreciosMinimos.
+   El objetivo es que el cursor recorra todos los productos y, si encuentra alguno cuyo precio_venta sea inferior a 0.50€,
+   lo actualice automáticamente a ese valor (0.50€). */
+
+drop procedure if exists dbsupermercado.pCorregirPreciosMinimos;
+delimiter $$
+create procedure dbSupermercado.pCorregirPreciosMinimos()
+begin
+    declare v_fin boolean default false;
+    declare v_precioventa float;
+    declare v_codigo int;
+    declare puntero cursor for select tblProductos.precio_venta, tblProductos.codigobarra
+        from dbsupermercado.tblProductos
+            where precio_venta < 0.50;
+    declare continue handler for not found set v_fin = true;
+    open puntero;
+    bucle: loop
+        fetch puntero into v_precioventa, v_codigo;
+        if v_fin then
+            leave bucle;
+        end if ;
+        update tblProductos
+        set precio_venta = 0.50
+        where codigobarra = v_codigo;
+    end loop ;
+    close puntero;
+end $$
+delimiter ;
+
+call dbSupermercado.pCorregirPreciosMinimos();
+
+
+
+
+
 
 
