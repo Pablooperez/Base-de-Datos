6c40bef839c3d1ef764242bd66ee791c2edae34b
pablo pérez <pablo.perez.martinez99@gmail.com>
2026-04-21 12:25:40 +0200

    Ejercicio Triggers: 2,3 y 4

--- dbSupermercado_Triggers_Ejercicios.sql
+++ dbSupermercado_Triggers_Ejercicios.sql
@@ -47,5 +47,27 @@ where codigo in (100001, 100002);
 
 select * from tbl_auditoria_precios;
 
+-- Ejercicio 2:
+-- Enunciado: Crear una rutina que impida establecer cantidades negativas en el stock de productos. Si intenta poner un valor negativo, se establece a 0.
+-- Analisis: Primero tenemos que ver sobre qué tabla estamos vigilando, y seguidamente sobre qué procesos tenemos que vigilar.
+    -- Tabla a vigilar: tblproductos Procesos a vigilar: Update, Insert.
+    -- Creamos set cantidad = new.tblproductos.cantidad
+    -- Si cantidad < 0 entonces new.tbl.productos.cantidad = 0
+    -- Si cantidad > 0 entonces new.tbl.productos.cantidad = cantidad
+-- Algoritmo:
+
+-- Ejercicio 3:
+-- Enunciado: Crear una rutina que al insertar un empleado asigne automaticamente la fecha de alta.
+-- Analisis: Primero tenemos que ver sobre qué tabla estamos vigilando, y seguidamente sobre qué procesos tenemos que vigilar.
+    -- Tabla a vigilar: tblempleados Procesos a vigilar: Insert.
+    -- Dado que dentro de tblempleados, no disponemos de una columna fecha_alta, debemos crearla.
+    -- Cuando se realiza un Insert dentro de tblempleados, automaticamente en fecha_alta = current_date().
+-- Algoritmo:
+
+-- Ejercicio 4:
+-- Enunciado: Crear una rutina automatizada que calcule automaticamente el precio de venta cuando se modifique el precio de coste o el IVA. Precio-Venta = (PrecioCoste+IVA)*1.30.
+-- Analisis:
+-- Algoritmo:
+
 
 
